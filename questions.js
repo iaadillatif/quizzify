f459b0f9fb6e942056dec4b69a9901e4f9dcfeed
Aadil Latif <aadillatif95230@gmail.com>
2023-12-03 09:58:56 +0530

    quiz_app

---
+++ questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following elements of electrical engineering cannot be analyzed using Ohm’s law?",
+    answer: "c) Transistors",
+    options: [
+      "a) Capacitors",
+      "b) Inductors",
+      "c) Transistors",
+      "d) Resistance"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is constant for a charged spherical shell according to basic electrical energy?",
+    answer: "b) Electrical potential inside the spherical shell",
+    options: [
+      "a) Electrical potential outside the spherical shell",
+      "b) Electrical potential inside the spherical shell",
+      "c) Electrical field outside the spherical shell",
+      "d) Electrical field inside the spherical shell"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where does electro-static shielding occur in a charged spherical shell?",
+    answer: "d) Electrical field inside the spherical shell",
+    options: [
+      "a) When electrical potential outside spherical shell is zero",
+      "b) When electrical potential inside the spherical shell is zero",
+      "c) When electrical field outside the spherical shell",
+      "d) Electrical field inside the spherical shell",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is a correct representation of peak value in an AC Circuit?",
+    answer: "d) RMS value*Peak factor",
+    options: [
+      "a) RMS value/Peak factor",
+      "b) RMS value*Form factor",
+      "c) RMS value/Form factor",
+      "d) RMS value*Peak factor"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following according to fundaments of electrical energy is correct about alternating current?",
+    answer: "a) Frequency is zero",
+    options: [
+      "a) Frequency is zero",
+      "b) Magnitude changes with time",
+      "c) Can be transported to larger distances with less loss in power",
+      "d) Flows in both directions"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How many cycles will an AC signal make in 2 seconds if its frequency is 100 Hz?",
+    answer: "d) 200",
+    options: [
+      "a) 50",
+      "b) 100",
+      "c) 150",
+      "d) 200"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What will be the direction of the drift velocity of electrons change with respect to the electric field?",
+    answer: "b) opposite to that of electric field",
+    options: [
+      "a) same as that of electric field",
+      "b) opposite to that of electric field",
+      "c) perpendicular to that of the electric field in a positive direction",
+      "d) perpendicular to that of the electric field in a negative direction"
+    ]
+  }
+  ,
+    {
+    numb: 8,
+    question: "What kind of quantity is an Electric potential?",
+    answer: "c) Scalar quantity",
+    options: [
+      "a) Vector quantity",
+      "b) Tensor quantity",
+      "c) Scalar quantity",
+      "d) Dimensionless quantity"
+    ]
+  }
+  ,
+    {
+    numb: 9,
+    question: "Which of the following is correct about the power consumed by R1 and R2 connected in series if the value of R1 is greater than R2?",
+    answer: "a) R1 will consume more power",
+    options: [
+      "a) R1 will consume more power",
+      "b) R2 will consume more power",
+      "c) R1 and R2 will consume the same power",
+      "d) The relationship between the power consumed cannot be established"
+    ]
+  }
+  ,
+    {
+    numb: 10,
+    question: "What is zero for a charged spherical shell?",
+    answer: "d) Electrical field inside the spherical shell",
+    options: [
+      "a) Electrical potential outside the spherical shell",
+      "b) Electrical potential inside the spherical shell",
+      "c) Electrical field outside the spherical shell",
+      "d) Electrical field inside the spherical shell"
+    ]
+  }
+  
+
+];
